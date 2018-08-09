093c3841ff6faa60bd4788becf67fb325d408205
Wichian Kraubut <wichian.kraubut@gmail.com>
2018-08-10 00:18:52 +0700

    Commit after can get connect from config file

---
+++ WorkshopCRUDAccess/Main.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using System.Configuration;
+namespace WorkshopCRUDAccess
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            //using(OleDbConnection con = new OleDbConnection(ConfigurationManager.ConnectionStrings.))
+            //   {
+            //       con.Open();
+            //       MessageBox.Show(con.State.ToString());
+            //   }
+            Console.WriteLine("Connection String : "+ConfigurationManager.ConnectionStrings[1]);
+        }
+    }
+}
